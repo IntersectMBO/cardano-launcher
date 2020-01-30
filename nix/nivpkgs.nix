6aea8ebb3c17a392e3e081e875b5af9d5999404b
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-01-30 12:20:32 +1000

    Add jormungandr configs from iohk-nix

--- nix/nivpkgs.nix
+++ nix/nivpkgs.nix
@@ -1,9 +1,11 @@
 { sources ? import ./sources.nix }:
 with
-  { overlay = _: pkgs:
+  { overlay = self: pkgs:
       { niv = import sources.niv {};
         cardanoWalletPackages = import sources.cardano-wallet {};
         cardano-node = (import sources.cardano-node {}).nix-tools.cexes.cardano-node.cardano-node;
+        inherit (import sources.iohk-nix {}) jormungandrLib;
+        jormungandrConfigs = self.jormungandrLib.forEnvironments self.jormungandrLib.mkConfigHydra;
       };
   };
 import sources.nixpkgs
