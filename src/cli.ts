587fd0c44e939bdd89a1d9b192ee2ac19039496f
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-03-31 19:41:49 +1100

    Restore function wrapping the cli behaviour

--- src/cli.ts
+++ src/cli.ts
@@ -13,8 +13,9 @@
  * @packageDocumentation
  */
 
-import process from 'process';
 import _ from 'lodash';
+import process from 'process';
+import Process = NodeJS.Process;
 
 import {
   Launcher,
@@ -25,66 +26,74 @@ import {
   ServiceExitStatus,
   serviceExitStatusMessage,
 } from './service';
-
 import * as byron from './byron';
 import * as jormungandr from './jormungandr';
 
-const waitForExit = setInterval(function() {}, 3600000);
-const args = process.argv;
-args.shift(); // /usr/bin/node
-args.shift(); // cardano-launcher
+/**
+ * Main function of the CLI.
+ *
+ * Is just a very basic interface for testing things.
+ */
+export function cli(argv: Process['argv']) {
+  const waitForExit = setInterval(function () {
+  }, 3600000);
+  const args = argv;
 
-if (args.length < 4) {
-  usage();
-}
+  args.shift(); // /usr/bin/node
+  args.shift(); // cardano-launcher
 
-const backend = args.shift() as string;
-const networkName = args.shift() as string;
-const configurationDir = args.shift() as string;
-const stateDir = args.shift() as string;
-
-let nodeConfig: any;
-
-if (backend === 'byron') {
-  if (!(networkName in byron.networks)) {
-    console.error(`unknown network: ${networkName}`);
-    process.exit(2);
+  if (args.length < 4) {
+    usage();
   }
-  const network = byron.networks[networkName];
-  nodeConfig = {
-    kind: backend,
-    configurationDir,
-    network,
-  };
-} else if (backend === 'jormungandr') {
-  if (!(networkName in jormungandr.networks)) {
-    console.error(`unknown network: ${networkName}`);
-    process.exit(2);
+
+  const backend = args.shift() as string;
+  const networkName = args.shift() as string;
+  const configurationDir = args.shift() as string;
+  const stateDir = args.shift() as string;
+
+  let nodeConfig: any;
+
+  if (backend === 'byron') {
+    if (!(networkName in byron.networks)) {
+      console.error(`unknown network: ${networkName}`);
+      process.exit(2);
+    }
+    const network = byron.networks[networkName];
+    nodeConfig = {
+      kind: backend,
+      configurationDir,
+      network,
+    };
+  } else if (backend === 'jormungandr') {
+    if (!(networkName in jormungandr.networks)) {
+      console.error(`unknown network: ${networkName}`);
+      process.exit(2);
+    }
+    const network = jormungandr.networks[networkName];
+    nodeConfig = {
+      kind: backend,
+      configurationDir,
+      network,
+    };
+  } else {
+    usage();
   }
-  const network = jormungandr.networks[networkName];
-  nodeConfig = {
-    kind: backend,
-    configurationDir,
-    network,
-  };
-} else {
-  usage();
+
+  const launcher = new Launcher({stateDir, nodeConfig, networkName}, console);
+
+  launcher.start();
+
+  // inform tests of subprocess pids
+  launcher.nodeService.start().then(pid => sendMaybe({node: pid}));
+  launcher.walletService.start().then(pid => sendMaybe({wallet: pid}));
+
+  launcher.walletBackend.events.on('exit', (status: ExitStatus) => {
+    console.log(serviceExitStatusMessage(status.wallet));
+    console.log(serviceExitStatusMessage(status.node));
+    clearInterval(waitForExit);
+    process.exit(combineStatus([status.wallet, status.node]));
+  });
 }
-
-const launcher = new Launcher({ stateDir, nodeConfig, networkName }, console);
-
-launcher.start();
-
-// inform tests of subprocess pids
-launcher.nodeService.start().then(pid => sendMaybe({ node: pid }));
-launcher.walletService.start().then(pid => sendMaybe({ wallet: pid }));
-
-launcher.walletBackend.events.on('exit', (status: ExitStatus) => {
-  console.log(serviceExitStatusMessage(status.wallet));
-  console.log(serviceExitStatusMessage(status.node));
-  clearInterval(waitForExit);
-  process.exit(combineStatus([status.wallet, status.node]));
-});
 
 function combineStatus(statuses: ServiceExitStatus[]): number {
   let code = _.reduce(
@@ -120,3 +129,5 @@ function sendMaybe(message: object) {
     process.send(message);
   }
 }
+
+cli(process.argv);
